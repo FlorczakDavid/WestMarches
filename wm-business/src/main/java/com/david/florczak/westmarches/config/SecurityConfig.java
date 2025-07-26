50c0a6d3255864442f530ba3e0ea3a07ba45528b
David FLORCZAK <florczakdavid@gmail.com>
2025-07-26 13:57:06 +0200

    added profile to back to manage CORS

--- wm-business/src/main/java/com/david/florczak/westmarches/config/SecurityConfig.java
+++ wm-business/src/main/java/com/david/florczak/westmarches/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,7 +36,7 @@ public class SecurityConfig {
 	@Value("${com.david.florczak.westmarches.BCrypt.rounds}")
 	private int rounds;
 	
-	@Value("${com.david.florczak.westmarches.cors}")
+	@Value("${com.david.florczak.westmarches.cors:}")
 	private String origins;
 
 	@Value("${com.david.florczak.westmarches.jwt.secret}")
@@ -86,6 +87,7 @@ public class SecurityConfig {
 	}
 	
 	@Bean
+	@Profile("dev")
 	SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
 		security
 			.csrf(csrf -> csrf.disable())
@@ -102,8 +104,6 @@ public class SecurityConfig {
 			.oauth2ResourceServer(oauth2 ->
 				oauth2.jwt(Customizer.withDefaults()))
 			.cors(Customizer.withDefaults());
-//			.cors(cors -> cors.disable());
-		
 		
 		return security.build();
 	}
